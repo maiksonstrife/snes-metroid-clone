9536786ea4f68450c87ed9353a6ead8e2f8b292f
crismand_dev <crismand-dev@outlook.com>
2020-05-27 19:03:13 -0400

    +Debug Scene +Shooting Power Beam +PlayerController refactor to refine movement/jumping

---
+++ SNES Metroid Clone/Assets/Scripts/Equipment/PowerSuit.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+
+namespace Equipment
+{
+    public class PowerSuit : MonoBehaviour
+    {
+        public enum SuitType
+        {
+            PowerSuit,
+            VariaSuit,
+            GravitySuit
+        };
+
+        public SuitType _suitType = SuitType.PowerSuit;
+        public enum Upgrade
+        {
+            XrayVisor,
+            MorphBall,
+            Bomb,
+            PowerBomb,
+            SpringBall,
+            HighJump,
+            SpeedBoost,
+            GrappleBeam,
+            SpaceJump,
+            ChargeBeam,
+            SpazerBeam,
+            IceBeam,
+            WaveBeam,
+            PlasmaBeam,
+            VariaSuit,
+            GravitySuit,
+            ScrewAttack
+        };
+
+        public bool _xrayVisorEnabled = false;
+        
+        public bool _morphballEnabled = false;
+        public bool _bombEnabled = false;
+        public bool _powerBombEnabled = false;
+        public bool _springBallEnabled = false;
+
+        public bool _highJumpEnabled = false;
+        public bool _speedboostEnabled = false;
+        public bool _grappleBeamEnabled = false;
+        public bool _spaceJumpEnabled;
+        
+        public bool _chargeBeamEnabled = false;
+        
+        public bool _spazerBeamEnabled = false;
+        public bool _iceBeamEnabled = false;
+        public bool _waveBeamEnabled = false;
+        public bool _plasmaBeamEnabled = false;
+
+        private bool _isVariaSuit = false;
+        private bool _isGravitySuit = false;
+        public bool _screwAttackEnabled = false;
+
+        public GameObject PowerBeam;
+        
+        public void Activate(Upgrade upgrade)
+        {
+            switch (upgrade)
+            {
+                case Upgrade.XrayVisor:
+                    _xrayVisorEnabled = true;
+                    break;
+                case Upgrade.MorphBall:
+                    _morphballEnabled = true;
+                    break;
+                case Upgrade.Bomb:
+                    _bombEnabled = true;
+                    break;
+                case Upgrade.PowerBomb:
+                    _powerBombEnabled = true;
+                    break;
+                case Upgrade.SpringBall:
+                    _springBallEnabled = true;
+                    break;
+                case Upgrade.HighJump:
+                    _highJumpEnabled = true;
+                    break;
+                case Upgrade.SpeedBoost:
+                    _speedboostEnabled = true;
+                    break;
+                case Upgrade.GrappleBeam:
+                    _grappleBeamEnabled = true;
+                    break;
+                case Upgrade.SpaceJump:
+                    _spaceJumpEnabled = true;
+                    break;
+                case Upgrade.ChargeBeam:
+                    _chargeBeamEnabled = true;
+                    break;
+                case Upgrade.SpazerBeam:
+                    _spazerBeamEnabled = true;
+                    break;
+                case Upgrade.IceBeam:
+                    _iceBeamEnabled = true;
+                    break;
+                case Upgrade.WaveBeam:
+                    _waveBeamEnabled = true;
+                    break;
+                case Upgrade.PlasmaBeam:
+                    _plasmaBeamEnabled = true;
+                    break;
+            }
+        }
+
+        public bool IsEnabled(Upgrade upgrade)
+        {
+            switch (upgrade)
+            {
+                case Upgrade.XrayVisor:
+                    return _xrayVisorEnabled;
+                case Upgrade.MorphBall:
+                    return _morphballEnabled;
+                case Upgrade.Bomb:
+                    return _bombEnabled;
+                case Upgrade.PowerBomb:
+                    return _powerBombEnabled;
+                case Upgrade.SpringBall:
+                    return _springBallEnabled;
+                case Upgrade.HighJump:
+                    return _highJumpEnabled;
+                case Upgrade.SpeedBoost:
+                    return _speedboostEnabled;
+                case Upgrade.GrappleBeam:
+                    return _grappleBeamEnabled;
+                case Upgrade.SpaceJump:
+                    return _spaceJumpEnabled;
+                case Upgrade.ChargeBeam:
+                    return _chargeBeamEnabled;
+                case Upgrade.SpazerBeam:
+                    return _spazerBeamEnabled;
+                case Upgrade.IceBeam:
+                    return _iceBeamEnabled;
+                case Upgrade.WaveBeam:
+                    return _waveBeamEnabled;
+                case Upgrade.PlasmaBeam:
+                    return _plasmaBeamEnabled;
+                default:
+                    return false;
+            }
+        }
+
+        public GameObject GetSelectedWeapon()
+        {
+            return PowerBeam;
+        }
+    }
+}
---
+++ SNES Metroid Clone/Assets/Scripts/Weapons/PowerBeam.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Weapons
+{
+ 
+    
+    
+    [RequireComponent(typeof(CircleCollider2D))]
+    public class PowerBeam : MonoBehaviour, IWeapon
+    {
+        [SerializeField] private int _damage = 10;
+        [SerializeField] private float _speed = 10.0f;
+        
+        private Vector3 _direction = Vector3.right;
+        private CircleCollider2D _collider;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            _collider = GetComponent<CircleCollider2D>();
+            StartCoroutine(SelfDestruct());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position += _direction * (_speed * Time.deltaTime);
+        }
+
+        public void SetDirection(Vector3 direction)
+        {
+            _direction = direction;
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if(other.gameObject.layer == 9)               //Layer 9 is platforms
+                Destroy(this.gameObject);
+        }
+
+        private IEnumerator SelfDestruct()
+        {
+            yield return new WaitForSeconds(5.0f);
+            Destroy(this.gameObject);
+        }
+    }
+}
