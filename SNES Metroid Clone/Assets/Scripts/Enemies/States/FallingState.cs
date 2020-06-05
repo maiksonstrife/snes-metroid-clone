246a7f56e1c9314f525c678289feb3b5e35c7821
crismand_dev <crismand-dev@outlook.com>
2020-06-04 23:06:22 -0400

    +Zoomer Prefab, buggy but working **When Zoomer on screen, power beam fails to detect collision with platforms... performance issue?

---
+++ SNES Metroid Clone/Assets/Scripts/Enemies/States/FallingState.cs
@@ -0,0 +1,40 @@
+﻿using Control;
+using UnityEngine;
+
+namespace Enemies.States
+{
+    public class FallingState : EnemyState
+    {
+        
+        
+        public FallingState(CrawlingBehavior fsm)
+        {
+            _root = fsm;
+            _CC2D = _root.GetComponent<CharacterController2D>();
+        }
+        public override void EnterState()
+        {
+            Debug.Log("Entered Falling State");
+        }
+
+        public override void Update()
+        {
+            Vector3 totalMove = Vector3.zero;
+
+            totalMove += _root.gravityDirection * (_root.gravity * Time.deltaTime);
+            _CC2D.Move(totalMove);
+            _root.collisionState = _CC2D.collisionState;
+
+            if (_root.IsGrounded())
+            {
+                _root.SetRotation(_root.gravityDirection);
+                _root.TransitionToState(_root.CrawlingState);
+            }
+        }
+
+        public override void ExitState()
+        {
+            //Debug.Log("Exited Falling State");
+        }
+    }
+}
